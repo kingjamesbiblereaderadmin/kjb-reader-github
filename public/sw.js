f0fd6db31ff8e3cdff906093da549502954c178f
Shawn Poh Hanlin <shawnlovesdeerrhorsesavi@gmail.com>
2026-08-18 22:40:22 +0000

    Increment service worker cache version

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
-// KJB Reader Service Worker v20260817_0040
+// KJB Reader Service Worker v20260819_0639
 // Cache-first loading for offline support
 
-const CACHE_NAME = 'kjb-reader-v20260817_0040';
+const CACHE_NAME = 'kjb-reader-v20260819_0639';
 const LEGACY_CACHE_NAME = 'kjb-legacy-v9';
 
 // Core app shell resources to cache immediately
