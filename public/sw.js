7152821d1dc5a82b1a72df987ff221946c83c72a
Shawn Poh Hanlin <shawnlovesdeerrhorsesavi@gmail.com>
2026-08-19 01:04:13 +0000

    Bump service worker version

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
-// KJB Reader Service Worker v20260819_0729
+// KJB Reader Service Worker v20260819_0903
 // Cache-first loading for offline support
 
-const CACHE_NAME = 'kjb-reader-v20260819_0729';
+const CACHE_NAME = 'kjb-reader-v20260819_0903';
 const LEGACY_CACHE_NAME = 'kjb-legacy-v9';
 
 // Core app shell resources to cache immediately
