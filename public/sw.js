caf7833feb53e8e2f9a21d917cce63753fb9d58e
Shawn Poh Hanlin <shawnlovesdeerrhorsesavi@gmail.com>
2026-08-23 03:57:26 +0000

    External agent changes

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
-// KJB Reader Service Worker v20260823_1530
+// KJB Reader Service Worker v20260823_1615
 // Cache-first loading for offline support
 
-const CACHE_NAME = 'kjb-reader-v20260823_1530';
+const CACHE_NAME = 'kjb-reader-v20260823_1615';
 const LEGACY_CACHE_NAME = 'kjb-legacy-v9';
 
 // Core app shell resources to cache immediately
