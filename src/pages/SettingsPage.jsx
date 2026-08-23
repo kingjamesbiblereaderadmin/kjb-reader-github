4a00b9e306192df652525dea9e504631c55e5bea
Shawn Poh Hanlin <shawnlovesdeerrhorsesavi@gmail.com>
2026-08-23 03:49:53 +0000

    External agent changes

--- src/pages/SettingsPage.jsx
+++ src/pages/SettingsPage.jsx
@@ -50,7 +50,7 @@ const isBookmarkBrowser = () => {
 };
 
 const LAST_REVISED = 'July 13th, 2026';
-const WORKER_VERSION = 'v20260823_1200';
+const WORKER_VERSION = 'v20260823_1400';
 
 export default function SettingsPage() {
   const navigate = useNavigate();
